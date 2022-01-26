a89ef1ca86a402451bc044472466e1326b271eed
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-01-26 23:12:55 +0200

    secondTask

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const itemsIngr = ingredients.map(ingredient => {
+  const el = document.createElement("li");
+  el.textContent = ingredient;
+  el.classList.add("item");
+  return el;
+});
+const listOfIngr = document.querySelector("#ingredients");
+listOfIngr.append(...itemsIngr);
