c99bb734567b8364d768dd94ba7d6680d648dab2
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-01-31 19:42:07 +0200

    ninthtask

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const colorRef = document.querySelector('.color');
+const btnColorRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+
+btnColorRef.addEventListener('click', onBtnClick);
+function onBtnClick() {
+  const newColor = getRandomHexColor();
+  colorRef.textContent = newColor;
+  bodyRef.style.backgroundColor = newColor;
 }
