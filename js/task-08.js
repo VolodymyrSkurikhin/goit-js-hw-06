7ff6181862dd113e7783f26af4ad43eea3655f1c
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-01-31 18:32:09 +0200

    eighthtask no console

--- js/task-08.js
+++ js/task-08.js
@@ -1,12 +1,10 @@
 const formRef = document.querySelector('.login-form');
 formRef.addEventListener('submit', onSubmitForm);
 function onSubmitForm(event,email,password) {
-  console.dir(formRef);
   event.preventDefault();
   email = event.currentTarget.elements.email.value;
   password  = event.currentTarget.elements.password.value;
 
-  console.dir(event.currentTarget);
   if (email === "" || password === "") {
     alert("Please, fill in all the required forms!");
   }
