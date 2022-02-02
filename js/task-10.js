c58aba9d246571b03c8f5de640e61ef4b0b333d8
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-02-02 23:44:36 +0200

    tenthtask

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputRef = document.querySelector('[type="number"]');
+const btnCreateRef = document.querySelector('[data-create]');
+const btnDestroyRef = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+btnCreateRef.addEventListener('click', createBoxes);
+
+function createBoxes(amount) {
+  const boxesToPut = [];
+  amount = Number(inputRef.value);
+  for (let i = 0; i < amount; i += 1) {
+
+    let newBoxRef = document.createElement("div");
+
+    newBoxRef.style.backgroundColor = getRandomHexColor();
+    newBoxRef.style.width = `${30 + 10 * i}px`;
+    newBoxRef.style.height = `${30 + 10 * i}px`;
+    boxesToPut.push(newBoxRef);
+  }
+  boxesRef.append(...boxesToPut);
+};
+
+btnDestroyRef.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  boxesRef.innerHTML = "";
+  inputRef.value = "";
+};
+
+
+
